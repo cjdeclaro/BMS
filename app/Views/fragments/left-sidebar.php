d51552ba32eeed8a276bb6b4fdf531b53ba27b6d
Harley Ferrer Lumagui <lumagui.harl0723@gmail.com>
2021-06-25 10:15:46 +0800

    Update Sidebar Link Items

--- app/Views/fragments/left-sidebar.php
+++ app/Views/fragments/left-sidebar.php
@@ -8,26 +8,26 @@
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
                         <!-- User Profile-->
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="<?= base_url() ?>" aria-expanded="false"><i class="me-3 far fa-clock fa-fw"
-                                    aria-hidden="true"></i><span class="hide-menu">Dashboard</span></a></li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="<?= base_url('reporting-person') ?>" aria-expanded="false">
-                                <i class="me-3 fa fa-user" aria-hidden="true"></i><span
-                                    class="hide-menu">Reporting Person</span></a>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= base_url() ?>" aria-expanded="false">
+                                <i class="me-3 far fa-clock fa-fw" aria-hidden="true"></i><span class="hide-menu">Dashboard</span>
+                            </a>
                         </li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="<?= base_url('suspect-data') ?>" aria-expanded="false"><i class="me-3 fa fa-table"
-                                    aria-hidden="true"></i><span class="hide-menu">Suspect Data</span></a></li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="<?= base_url('victim-data') ?>" aria-expanded="false"><i class="me-3 fa fa-font"
-                                    aria-hidden="true"></i><span class="hide-menu">Victim Data</span></a></li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="<?= base_url('narrative-of-incidents') ?>" aria-expanded="false"><i class="me-3 fa fa-columns"
-                                    aria-hidden="true"></i><span class="hide-menu">Narrative of Incidents</span></a></li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="<?= base_url('reports') ?>" aria-expanded="false"><i class="me-3 fa fa-info-circle"
-                                    aria-hidden="true"></i><span class="hide-menu">Reports</span></a></li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                                <i class="me-3 fa fa-user" aria-hidden="true"></i>
+                                <span class="hide-menu">Blotter</span>
+                            </a>
+                            <ul aria-expanded="false" class="collapse first-level">
+                                <li class="sidebar-item"><a href="<?= base_url() ?>" class="sidebar-link" style="padding-left: 20px"><i class="fa fa-plus"></i><span class="hide-menu"> Add New </span></a></li>
+                                <li class="sidebar-item"><a href="<?= base_url() ?>" class="sidebar-link" style="padding-left: 20px"><i class="fa fa-list"></i><span class="hide-menu"> View All </span></a></li>
+                            </ul>
+                        </li>
+                        <li class="sidebar-item"> 
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= base_url('reports') ?>" aria-expanded="false">
+                                <i class="me-3 fa fa-info-circle" aria-hidden="true"></i><span class="hide-menu">Reports</span>
+                            </a>
+                        </li>
                     </ul>
 
                 </nav>
